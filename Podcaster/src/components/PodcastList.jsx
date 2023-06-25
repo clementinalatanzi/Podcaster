062d048a6f0a778deefcab4b9a81d9434ebe373d
Clementina Latanzi <clementinalatanzi@gmail.com>
2023-06-25 17:34:39 -0300

    add filter and unit test for filter Update Header.jsx, Header.test.jsx, PodcastList.jsx, and 1 more file

--- Podcaster/src/components/PodcastList.jsx
+++ Podcaster/src/components/PodcastList.jsx
@@ -4,6 +4,7 @@ import { fetchPodcasts } from '../services/itunesClient';
 
 export function PodcastList() {
   const [podcasts, setPodcasts] = useState([]);
+  const [filterText, setFilterText] = useState('');
 
   useEffect(() => {
     const loadPodcasts = async () => {
@@ -18,11 +19,25 @@ export function PodcastList() {
     loadPodcasts();
   }, []);
 
+  const filteredPodcasts = podcasts.filter(podcast => {
+    const titleMatch = podcast['im:name']?.label.toLowerCase().includes(filterText.toLowerCase());
+    const authorMatch = podcast['im:artist']?.label.toLowerCase().includes(filterText.toLowerCase());
+    return filterText === '' || titleMatch || authorMatch;
+});
+
   return (
     <>
+      <div className='filter-container'>
+        <input
+          type="text"
+          value={filterText}
+          onChange={e => setFilterText(e.target.value)}
+          placeholder="Buscar podcasts..."
+        />
+      </div>
       <h1>PodcastList</h1>
       <ul>
-        {podcasts.map((podcast) => (
+        {filteredPodcasts.map((podcast) => (
           <section key={podcast.id.label} data-testid="podcast">
             <div className="section-overlay"></div>
             <div className="section-container">
@@ -31,7 +46,7 @@ export function PodcastList() {
                 title={podcast['im:name']?.label}
                 author={podcast['im:artist']?.label}
                 urlImage={podcast['im:image']?.[2]?.label}
-                
+
               />
             </div>
           </section>
--- Podcaster/src/components/PodcastList.test.js
+++ Podcaster/src/components/PodcastList.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import '@testing-library/jest-dom/extend-expect';
-import { render, screen, waitFor } from '@testing-library/react';
+import { render, screen, waitFor,fireEvent } from '@testing-library/react';
 import { PodcastList } from './PodcastList';
 
 jest.mock('../services/itunesClient', () => ({
@@ -74,4 +74,40 @@ describe('PodcastList', () => {
       );
     });
   });
+
+  test('filters podcasts by  author and title', async () => {
+    render(<PodcastList />);
+  
+    await waitFor(() => {
+      const podcastElements = screen.getAllByTestId('podcast');
+      expect(podcastElements).toHaveLength(2);
+    });
+  
+    fireEvent.change(screen.getByPlaceholderText('Buscar podcasts...'), { target: { value: 'Podcast 1' } });
+  
+    await waitFor(() => {
+      const podcastElements = screen.getAllByTestId('podcast');
+      expect(podcastElements).toHaveLength(1);
+      expect(podcastElements[0]).toHaveTextContent('Podcast 1');
+      expect(podcastElements[0]).toHaveTextContent('Author 1');
+      expect(podcastElements[0].querySelector('img')).toHaveAttribute(
+        'src',
+        'https://is1-ssl.mzstatic.com/image/thumb/Podcasts113/v4/f2/21/fa/f221fabd-017f-5125-633b-f1fe4f39802a/mza_182995249085044287.jpg/170x170bb.png'
+      );
+    });
+  
+    fireEvent.change(screen.getByPlaceholderText('Buscar podcasts...'), { target: { value: 'Author 2' } });
+  
+    await waitFor(() => {
+      const podcastElements = screen.getAllByTestId('podcast');
+      expect(podcastElements).toHaveLength(1);
+      expect(podcastElements[0]).toHaveTextContent('Podcast 2');
+      expect(podcastElements[0]).toHaveTextContent('Author 2');
+      expect(podcastElements[0].querySelector('img')).toHaveAttribute(
+        'src',
+        'https://is1-ssl.mzstatic.com/image/thumb/Podcasts113/v4/f2/21/fa/f221fabd-017f-5125-633b-f1fe4f39802a/mza_182995249085044287.jpg/170x170bb.png'
+      );
+    });
+  });
+  
 });

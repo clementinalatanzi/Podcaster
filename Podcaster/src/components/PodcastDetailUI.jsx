a46cb84b65509ff9e9635615e18b01749834c71f
Clementina Latanzi <clementinalatanzi@gmail.com>
2023-06-27 11:39:46 -0300

    adding css for PodcastDetail Update PodcastDetailUI.css and PodcastDetailUI.jsx

--- Podcaster/src/components/PodcastDetailUI.jsx
+++ Podcaster/src/components/PodcastDetailUI.jsx
@@ -1,41 +1,46 @@
 import React from 'react';
 import { calculateTimeInMinutes, formatDate } from '../utils/Date/formatTime';
 import { Link } from 'react-router-dom';
-
+import './PodcastDetailUI.css'
 const PodcastDetailUI = ({ episodes, urlImage, title, description, id }) => {
   return (
     <div>
-      <div>
-        <h1 className='border-container '> EPISODES: {episodes.length}</h1>
-        <div className='border-container'>
-          <table>
-            <thead>
-              <tr>
-                <th>Title</th>
-                <th>Date</th>
-                <th>Duration</th>
-              </tr>
-            </thead>
-            <tbody>
-              {episodes.map(episodio => (
-                <tr key={episodio.trackId}>
-                  <td>
-                    <Link to={`/podcast/${id}/episode/${episodio.trackId}`}
-                      state={{
-                        urlImage: urlImage,
-                        title: title,
-                        description: description,
-                        id:id
-                      }}>
-                      {episodio.trackName}
-                    </Link>
-                  </td>
-                  <td>{formatDate(episodio.releaseDate)}</td>
-                  <td>{calculateTimeInMinutes(episodio.trackTimeMillis)}</td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
+      <div className="border-container">
+        <h1>EPISODES: {episodes.length}</h1>
+        </div>
+        <div className="border-container">
+        <div className="episode-table-list">
+          <div className="episode-table-item titles">
+            <div className="episode-table-title">Title</div>
+            <div className="episode-table-details">
+              <div>Date</div>
+              <div>Duration</div>
+            </div>
+          </div>
+          {episodes.map((episodio, index) => (
+            <div
+              key={episodio.trackId}
+              className={`episode-table-item ${index % 2 === 0 ? 'even' : 'odd'}`}
+            >
+              <div className="episode-table-title">
+                <Link
+                  to={`/podcast/${id}/episode/${episodio.trackId}`}
+                  state={{
+                    urlImage: urlImage,
+                    title: title,
+                    description: description,
+                    id: id,
+                  }}
+                >
+                  {episodio.trackName}
+                </Link>
+              </div>
+              <div className="episode-table-details">
+                <div>{formatDate(episodio.releaseDate)}</div>
+                <div>{calculateTimeInMinutes(episodio.trackTimeMillis)}</div>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
     </div>

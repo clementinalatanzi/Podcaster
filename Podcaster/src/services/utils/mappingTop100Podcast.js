4daabc7ffea2052ec7e58bf2479e03d6b477b356
Clementina Latanzi <clementinalatanzi@gmail.com>
2023-06-27 19:47:15 -0300

    delete console.log

--- Podcaster/src/services/utils/mappingTop100Podcast.js
+++ Podcaster/src/services/utils/mappingTop100Podcast.js
@@ -1,8 +1,5 @@
 export function mappingTop100Podcast(Top100Podcast) {
-
-    //TODO console.info hacerlo solo para modo dev
-        console.info("mappingData data: ", Top100Podcast)
-     
+ 
         const podcastInfo = Top100Podcast.map(podcast => ({
             author: podcast['im:artist'].label,
             title: podcast.title.label.split("-")[0],
@@ -10,6 +7,6 @@ export function mappingTop100Podcast(Top100Podcast) {
             description: podcast.summary.label.split(".")[0]+".",
             id: podcast.id.attributes['im:id']
         }))
-        console.info("mappingData podcast: ", podcastInfo)
+        
         return podcastInfo
     }

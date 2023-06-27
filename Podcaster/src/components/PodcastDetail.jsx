f682c82b17a575707b359343eb165a37fabd701f
Clementina Latanzi <clementinalatanzi@gmail.com>
2023-06-27 18:07:21 -0300

    add error when external api fail  Update PodcastDetail.css and PodcastDetail.jsx

--- Podcaster/src/components/PodcastDetail.jsx
+++ Podcaster/src/components/PodcastDetail.jsx
@@ -8,31 +8,35 @@ import './PodcastDetail.css'
 export function PodcastDetail({setIsLoading}) {
 
   const location = useLocation();
+  const [error, setError] = useState(false)
   const { urlImage, title, description } = location.state || {};
-
+  
 
   const { id } = useParams();
 
-  const [podcastDetail, setPodcastDetail] = useState(null);
-
+  const [podcastDetail, setPodcastDetail] = useState(false);
+  
+  
   useEffect(() => {
     
     const fetchPodcastDetail = async () => {
       try {
         setIsLoading(true)
+        setError(false)
         const podcastData = await fetchPodcastData(id);
         setIsLoading(false)
         setPodcastDetail(podcastData);
       } catch (error) {
         setIsLoading(false)
         console.error("Unable to fetch podcast details", error)  
+        setError(true)
       }
     };
 
     fetchPodcastDetail();
   }, []);
-
-
+  
+ 
   return (
     podcastDetail ? (
       <div className='podcast-detail-container'>
@@ -42,8 +46,13 @@ export function PodcastDetail({setIsLoading}) {
         <div className='podcast-episodes'>
         <PodcastEpisodes episodes={podcastDetail.episodes} urlImage={urlImage} title={title} description={description} id={id} />
         </div>
-      </div>) :
-      <div></div>
+      </div>) :(error ? (
+        <div>
+        <h3 className='error-text'>¡Ups!</h3>
+        <h3 className='error-text'>Unfortunately, the podcast information could not be loaded at the moment.</h3>
+        <h3 className='error-text'>Please try again later.</h3>
+      </div>) : <div></div>)
+    
 
   )
 }
